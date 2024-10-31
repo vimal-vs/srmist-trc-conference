7e3423fac302478dc9b9f10ffc3329d6f67b475a
Vimal Vs <60288021+vimal-vs@users.noreply.github.com>
2024-10-31 11:51:35 +0530

    button fixed

--- src/app/page.js
+++ src/app/page.js
@@ -21,13 +21,13 @@ export default function Home() {
       {/* <FeeDetails /> */}
       <RegistrationDetails />
       <div className="container mx-auto px-4 py-8">
-            <h2 className="text-2xl font-semibold text-black mb-6">Downloads</h2>
-            <div className="space-y-4">
-                <div className="flex items-center space-x-2">
+            <h1 className="text-2xl md:text-3xl font-semibold text-blue-900 text-center w-fit pb-8 uppercase">DOWN<span className="text-yellow">LOADS</span></h1>
+            <div className="space-y-4 w-fit">
+                <div className="flex items-center space-x-2 w-fit">
                     <span>For paper template -</span>
                     <a
                         href="https://docs.google.com/document/d/1A0wjz1Xv1Njg5vtHmXYoQYdYvGd8eDVm/edit?usp=sharing&ouid=106267889453596678336&rtpof=true&sd=true"
-                        className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition"
+                        className="px-4 py-2 bg-blue-900 text-white font-semibold rounded-full hover:bg-blue-700 transition"
                     >
                         MANUSCRIPT
                     </a>
@@ -36,7 +36,7 @@ export default function Home() {
                     <span>For copyright form -</span>
                     <a
                         href="https://drive.google.com/file/d/1SGQt_naxdvGq0BePN_Qc740W-1QvNIXu/view?usp=sharing"
-                        className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition"
+                        className="px-4 py-2 bg-blue-900 text-white font-semibold rounded-full hover:bg-blue-700 transition"
                     >
                         COPYRIGHT FORM
                     </a>
@@ -45,7 +45,7 @@ export default function Home() {
                     <span>For publication ethics -</span>
                     <a
                         href="https://conference.poornima.edu.in/downloads/Publication%20Ethics.docx.pdf"
-                        className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition"
+                        className="px-4 py-2 bg-blue-900 text-white font-semibold rounded-full hover:bg-blue-700 transition"
                     >
                         PUBLICATION ETHICS
                     </a>
@@ -54,7 +54,7 @@ export default function Home() {
                     <span>For article preparation checklist (Do&apos;s & Don&apos;ts) -</span>
                     <a
                         href="https://drive.google.com/file/d/1Y_JW9jcmITdPSWbQkPvNNRKvoms5oOZH/view?usp=sharing"
-                        className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition"
+                        className="px-4 py-2 bg-blue-900 text-white font-semibold rounded-full hover:bg-blue-700 transition"
                     >
                         DO&apos;S & DON&apos;TS
                     </a>
