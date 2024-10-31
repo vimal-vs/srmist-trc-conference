a6c0cb1d7595942800631855b2f2c9981bb95094
Vimal Vs <60288021+vimal-vs@users.noreply.github.com>
2024-10-31 11:45:29 +0530

    downloads added

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,47 @@ export default function Home() {
       <ProgramSchedule />
       {/* <FeeDetails /> */}
       <RegistrationDetails />
+      <div className="container mx-auto px-4 py-8">
+            <h2 className="text-2xl font-semibold text-black mb-6">Downloads</h2>
+            <div className="space-y-4">
+                <div className="flex items-center space-x-2">
+                    <span>For paper template -</span>
+                    <a
+                        href="https://docs.google.com/document/d/1A0wjz1Xv1Njg5vtHmXYoQYdYvGd8eDVm/edit?usp=sharing&ouid=106267889453596678336&rtpof=true&sd=true"
+                        className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition"
+                    >
+                        MANUSCRIPT
+                    </a>
+                </div>
+                <div className="flex items-center space-x-2">
+                    <span>For copyright form -</span>
+                    <a
+                        href="https://drive.google.com/file/d/1SGQt_naxdvGq0BePN_Qc740W-1QvNIXu/view?usp=sharing"
+                        className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition"
+                    >
+                        COPYRIGHT FORM
+                    </a>
+                </div>
+                <div className="flex items-center space-x-2">
+                    <span>For publication ethics -</span>
+                    <a
+                        href="https://conference.poornima.edu.in/downloads/Publication%20Ethics.docx.pdf"
+                        className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition"
+                    >
+                        PUBLICATION ETHICS
+                    </a>
+                </div>
+                <div className="flex items-center space-x-2">
+                    <span>For article preparation checklist (Do's & Don'ts) -</span>
+                    <a
+                        href="https://drive.google.com/file/d/1Y_JW9jcmITdPSWbQkPvNNRKvoms5oOZH/view?usp=sharing"
+                        className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition"
+                    >
+                        DO'S & DON'TS
+                    </a>
+                </div>
+            </div>
+      </div>
       <VenueLocation />
       <MeetSpeakers />
     </div>
